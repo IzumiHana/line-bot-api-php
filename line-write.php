5bbfca387fd8c9d01a0edccb61631f3488362395
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 14:13:03 +0700

    Update line-write.php

--- line-write.php
+++ line-write.php
@@ -1,9 +1,17 @@
 <?php
-$param = $_REQUEST['param1'];
-$my_file = 'output.txt';
-$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-//$data = 'This is the data';
-$datetime = new DateTime();
-//$data = $datetime->format('Y-m-d H:i:s');
-fwrite($handle, "value ".$param);
+$request = file_get_contents('php://input');
+$request_array = json_decode($request, true);   // Decode JSON to Array
+if ( sizeof($request_array['param1']) > 0 )
+{
+  foreach ($request_array['events'] as $event)
+  {
+    $my_file = 'output.txt';
+    $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+    //$data = 'This is the data';
+    $datetime = new DateTime();
+    //$data = $datetime->format('Y-m-d H:i:s');
+    fwrite($handle, "value ".$param);
+  }
+}
+
 ?>
