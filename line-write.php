8189a11f142ca8c9d57e659e0e4b27967c6d7d0b
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 14:21:41 +0700

    Update line-write.php

--- line-write.php
+++ line-write.php
@@ -3,14 +3,14 @@ $request = file_get_contents('php://input');
 $request_array = json_decode($request, true);   // Decode JSON to Array
 if ( sizeof($request_array['param1']) > 0 )
 {
-  foreach ($request_array['events'] as $event)
+  foreach ($request_array['param1'] as $param1)
   {
     $my_file = 'output.txt';
     $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
     //$data = 'This is the data';
     $datetime = new DateTime();
     //$data = $datetime->format('Y-m-d H:i:s');
-    fwrite($handle, "value ".$param);
+    fwrite($handle, "value ".$param1);
   }
 }
 
