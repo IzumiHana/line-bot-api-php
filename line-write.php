abf0fd192842501760b814f4d73ca902d26bbf82
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 13:20:02 +0700

    Update line-write.php

--- line-write.php
+++ line-write.php
@@ -1,8 +1,9 @@
 <?php
+$param = $_REQUEST['param1'];
 $my_file = 'output.txt';
 $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
 //$data = 'This is the data';
 $datetime = new DateTime();
-$data = $datetime->format('Y-m-d H:i:s');
-fwrite($handle, $data);
+//$data = $datetime->format('Y-m-d H:i:s');
+fwrite($handle, $param);
 ?>
