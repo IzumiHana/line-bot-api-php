8416a2d7eddea04a619812765d9340c7890e787f
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 11:54:19 +0700

    Create line-write.php

---
+++ line-write.php
@@ -0,0 +1,8 @@
+<?php
+$my_file = 'output.txt';
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+//$data = 'This is the data';
+$datetime = new DateTime();
+$data = $datetime->format('Y-m-d H:i:s');
+fwrite($handle, $data);
+?>
