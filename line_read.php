5872f8b2dea5006e6f8b5d811a2416bdfb5fa748
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 15:29:26 +0700

    Update line_read.php

--- line_read.php
+++ line_read.php
@@ -1,7 +1,9 @@
 <?php
 
-$accessToken = 'oZZlYsAkX5Ij/sV2iXDw7MwpK5UT0x5GmWAVdI10fHfrOXpfh5zGmMCxb5nNNkfR9TE+rUVqjX9JeDWWJmEbWtQMTrdIfG1GxHjIEDjn/f8whb+9MmKoiHD68mXE72sOMAa3GTSJrM2lTk/3t/EfJgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
-$channelSecret = "e49746bfa8889c84066470dd0eba81ca";
+
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = 'oZZlYsAkX5Ij/sV2iXDw7MwpK5UT0x5GmWAVdI10fHfrOXpfh5zGmMCxb5nNNkfR9TE+rUVqjX9JeDWWJmEbWtQMTrdIfG1GxHjIEDjn/f8whb+9MmKoiHD68mXE72sOMAa3GTSJrM2lTk/3t/EfJgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $content = file_get_contents('php://input');
 $arrayJson = json_decode($content, true);
@@ -10,21 +12,46 @@ echo "TEST";
 $message = $arrayJson['events'][0]['message']['text'];
 
 
-function echoMessage($_accessToken,$_channelSecret,$_id,$_message)
-   {
-		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_accessToken);
-		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_channelSecret]);
-		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($_message);
-		$response = $bot->pushMessage($_id, $textMessageBuilder);
-		echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-   }
 
-if($message != "")
+function send_reply_message($url, $post_header, $post_body)
 {
-  $my_file = 'output.txt';
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+ return $result;
+}
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+if ( sizeof($request_array['events']) > 0 )
+{
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+	 
+	   $my_file = 'output.txt';
   $handle = fopen($my_file, 'r') or die('Cannot open file:  '.$my_file);
   $contents = fread($handle, filesize($my_file));
 	fclose($handle);
-	echoMessage($accessToken,$channelSecret,$id,$contents); 
+	
+$data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $contents]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+	
+ }
 }
+
+
+
+	
+
 ?>
