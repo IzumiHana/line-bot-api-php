8db7e7780c931025cbc2e9af547aa4ff17cb6938
IzumiHana <45029310+IzumiHana@users.noreply.github.com>
2019-07-26 09:53:08 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "Izumi/"; //enter your appid
+		$KEY = "oEzw9D1qO92QIs9"; //enter your web key
+		$SECRET = "1uSgbgoFpY2UFwpE3opCuRwn0"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
